4102fe2df336c70a7a5aba2b3e75bccc63a88932
Valentyn <maleyev.valentin@gmail.com>
2024-06-23 14:26:28 +0300

    Refactor to use Streams & Optional

--- src/main/java/com/magicvet/component/EntityRegister.java
+++ src/main/java/com/magicvet/component/EntityRegister.java
@@ -7,9 +7,10 @@ import main.java.com.magicvet.service.ClientService;
 import main.java.com.magicvet.service.PetService;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class EntityRegister {
 
@@ -21,13 +22,13 @@ public class EntityRegister {
 
         String message = "Do you want to register more clients? (y/n): ";
         do {
-            Client client = addClient();
-            if (client != null) {
-                clients.add(client);
-            }
+            Optional<Client> client = addClient();
+            client.ifPresent(clients::add);
         } while (verifyRepeating(message));
 
-        Map<Client.Location, List<Client>> clientsByLocation = groupClients(clients);
+        Map<Client.Location, List<Client>> clientsByLocation = clients.stream()
+                .collect(Collectors.groupingBy(Client::getLocation));
+
         printClients(clientsByLocation);
     }
 
@@ -41,69 +42,30 @@ public class EntityRegister {
         }
     }
 
-    private Map<Client.Location, List<Client>> groupClients(List<Client> clients) {
-        List<Client> fromKyiv = new ArrayList<>();
-        List<Client> fromKharkiv = new ArrayList<>();
-        List<Client> fromOdesa = new ArrayList<>();
-        List<Client> fromLviv = new ArrayList<>();
-        List<Client> unknownLocation = new ArrayList<>();
-
-        for (Client client : clients) {
-            switch (client.getLocation()) {
-                case KYIV -> fromKyiv.add(client);
-                case KHARKIV -> fromKharkiv.add(client);
-                case ODESA -> fromOdesa.add(client);
-                case LVIV -> fromLviv.add(client);
-                case UNKNOWN -> unknownLocation.add(client);
-            }
-        }
-
-        Map<Client.Location, List<Client>> clientsByLocation = new HashMap<>();
-        clientsByLocation.put(Client.Location.KYIV, fromKyiv);
-        clientsByLocation.put(Client.Location.KHARKIV, fromKharkiv);
-        clientsByLocation.put(Client.Location.ODESA, fromOdesa);
-        clientsByLocation.put(Client.Location.LVIV, fromLviv);
-        clientsByLocation.put(Client.Location.UNKNOWN, unknownLocation);
-
-        return clientsByLocation;
-    }
-
-    private Client addClient() {
-        Client client = clientService.registerNewClient();
-
-        if (client != null) {
-            System.out.print("Do you want to register your pet? (y/n): ");
-            String clientAnswer = Main.SCANNER.nextLine();
-
-            switch (clientAnswer) {
-                case "y" -> registerPets(client);
-                case "n" -> System.out.println("Pet has not been added.");
-            }
-            System.out.println(client);
-        } else {
-            System.out.println("Registration canceled.");
-        }
+    private Optional<Client> addClient() {
+        Optional<Client> client = clientService.registerNewClient();
+        client.ifPresent(this::registerPets);
 
         return client;
     }
 
     private void registerPets(Client client) {
         String message = "Do you want to add more pets for the current client? (y/n): ";
-
         do {
             addPet(client);
+            System.out.println(client);
         } while (verifyRepeating(message));
     }
 
     private void addPet(Client client) {
         System.out.println("Adding a new pet.");
 
-        Pet pet = petService.registerNewPet();
-        if (pet != null) {
-            client.addPet(pet);
-            pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
+        Optional<Pet> pet = petService.registerNewPet();
+        pet.ifPresent(value -> {
+            client.addPet(value);
+            value.setOwnerName(client.getFirstName() + " " + client.getLastName());
             System.out.println("Pet has been added.");
-        }
+        });
     }
 
     private boolean verifyRepeating(String message) {
